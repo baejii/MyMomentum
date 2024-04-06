3ec7c43e9ea537a78f7bad0b371f47379ce1ab9e
baejii <jiyn54311@gmail.com>
2024-04-07 02:26:07 +0900

    getDay() -> getDate()

--- js/date.js
+++ js/date.js
@@ -6,7 +6,7 @@ function getDate() {
 
     const years = String(dates.getFullYear());
     const months = String(dates.getMonth()+1).padStart(2, "0"); // getMonth()는 0~11 범위의 값으로 리턴하기 때문에 1을 더해줘야 함
-    const days = String(dates.getDay()).padStart(2, "0");
+    const days = String(dates.getDate()).padStart(2, "0");
     const dayOfWeek = week[dates.getDay()];
 
     date.innerText = `${years} ${months} ${days} ${dayOfWeek}`;
