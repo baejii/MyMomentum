61a5e8152e937d6df1195509507b7091b4e85ec3
baejii <jiyn54311@gmail.com>
2024-04-07 00:17:59 +0900

    fix: ampm

--- js/clock.js
+++ js/clock.js
@@ -6,10 +6,14 @@ function getClock() {
     const minutes = String(date.getMinutes()).padStart(2, "0");
     const ampm = date.getHours();
     
-    if (ampm >= 12) {
+    if (ampm > 12) { // 시간(0 ~ 23)
         clock.innerText = `${hours - 12} : ${minutes} PM`;
+    } else if (ampm === 12) {
+        clock.innerText = `12 : ${minutes} PM`;
+    } else if (ampm === 0) {
+        clock.innerText = `12 : ${minutes} AM`;
     } else {
-        clock.innerText = Math.abs(hours - 12) + ` : ${minutes} AM`;
+        clock.innerText = `${hours} : ${minutes} AM`;
     }
 }
 
